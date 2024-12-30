ca8a479e00dad8b2a03447c08b39d3fd5e8e3f1d
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2024-12-30 13:51:22 -0300

    Iniciando o projeto

---
+++ bin/dio_lab_trilha_dart_calculadora_imc.dart
@@ -0,0 +1,6 @@
+import 'package:dio_lab_trilha_dart_calculadora_imc/dio_lab_trilha_dart_calculadora_imc.dart'
+    as dio_lab_trilha_dart_calculadora_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dio_lab_trilha_dart_calculadora_imc.calculate()}!');
+}
