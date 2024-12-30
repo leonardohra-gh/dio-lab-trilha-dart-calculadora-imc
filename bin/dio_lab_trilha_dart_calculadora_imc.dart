a4dc00116bed8b9296d0c6e16f22a6ed0dfa5be2
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2024-12-30 14:43:05 -0300

    implementação do cálculo de IMC com checagem de erros

--- bin/dio_lab_trilha_dart_calculadora_imc.dart
+++ bin/dio_lab_trilha_dart_calculadora_imc.dart
@@ -1,6 +1,40 @@
-import 'package:dio_lab_trilha_dart_calculadora_imc/dio_lab_trilha_dart_calculadora_imc.dart'
-    as dio_lab_trilha_dart_calculadora_imc;
+import "dart:io";
+
+import "package:dio_lab_trilha_dart_calculadora_imc/pessoa.dart";
 
 void main(List<String> arguments) {
-  print('Hello world: ${dio_lab_trilha_dart_calculadora_imc.calculate()}!');
+  try {
+    Pessoa pessoa = pedirPessoaParaUsuario();
+    print(
+        "O IMC calculado com as informações passadas é de: ${pessoa.getIMC()}");
+  } on ArgumentError catch (e) {
+    print(e.message);
+  }
+}
+
+Pessoa pedirPessoaParaUsuario() {
+  String mensagemPeso = "Digite seu peso em kilos (Exemplo: 80.3):";
+  double pesoEmKg = pedirDouble(mensagemPeso);
+  String mensagemAltura = "Digite sua altura em metros (Exemplo: 1.83):";
+  double alturaEmMetros = pedirDouble(mensagemAltura);
+
+  return Pessoa(pesoEmKg, alturaEmMetros);
+}
+
+double pedirDouble(String mensagem) {
+  String valorinvalido = "Valor digitado inválido";
+  print(mensagem);
+
+  String? valorDigitado = stdin.readLineSync();
+
+  if (valorDigitado == null) {
+    throw ArgumentError(valorinvalido);
+  } else {
+    double? valorDouble = double.tryParse(valorDigitado);
+    if (valorDouble == null) {
+      throw ArgumentError(valorinvalido);
+    } else {
+      return valorDouble;
+    }
+  }
 }
---
+++ lib/pessoa.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+class Pessoa {
+  final double _pesoEmKg;
+  final double _alturaEmMetros;
+  double _imc = 0;
+
+  Pessoa(this._pesoEmKg, this._alturaEmMetros) {
+    if (_pesoEmKg <= 0) {
+      throw ArgumentError("Peso inválido");
+    }
+
+    if (_alturaEmMetros <= 0) {
+      throw ArgumentError("Altura inválida");
+    }
+
+    _imc = _pesoEmKg / pow(_alturaEmMetros, 2);
+  }
+
+  String getIMC() {
+    return "${_imc.toStringAsFixed(1)} (${_getFaixaIMC()})";
+  }
+
+  String _getFaixaIMC() {
+    switch (_imc) {
+      case > 40:
+        return "Obesidade grau III";
+      case > 35:
+        return "Obesidade grau II";
+      case > 30:
+        return "Obesidade grau I";
+      case > 25:
+        return "Sobrepeso";
+      case > 18.6:
+        return "Normal";
+      case <= 18.5:
+        return "Abaixo do normal";
+      default:
+        return "Não sei nem como você chegou aqui";
+    }
+  }
+}
