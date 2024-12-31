e1d94599ab5c2d123269e7e19e0615431340f3b4
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2024-12-31 12:37:12 -0300

    Adicionando testes

--- test/pessoa_test.dart
+++ test/pessoa_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:dio_lab_trilha_dart_calculadora_imc/pessoa.dart';
 import 'package:test/test.dart';
 
@@ -7,6 +5,16 @@ void main() {
   test("testar imc valor", () {
     Pessoa p = Pessoa(86, 1.85);
     double imc = p.getImc();
+    String imcFaixa = p.getFaixaIMC();
     expect(imc.toStringAsFixed(1), "25.1");
+    expect(imcFaixa, equals("Sobrepeso"));
+  });
+
+  test("testar peso inválido", () {
+    expect(() => Pessoa(-1, 1.85), throwsA(TypeMatcher<ArgumentError>()));
+  });
+
+  test("testar altura inválida", () {
+    expect(() => Pessoa(80, 0), throwsA(TypeMatcher<ArgumentError>()));
   });
 }
